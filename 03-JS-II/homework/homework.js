352ac05f4ae1e65ea9a0067e0625666640cb404d
Santiago Rosso <santiagorosso@live.com.ar>
2022-08-10 12:54:30 -0300

    29/36

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -56,7 +56,7 @@ function saludo(idioma) {
   }else if(idioma === "ingles") {
     return "Hello!";
   }else {
-    return "Hola!"
+    return "Hola!";
   }
 }
 
@@ -111,14 +111,27 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  
-}
+  if (numero % 0.5 === 0 && numero !== 0.5) {
+    return true;
+  } else {
+    return false;
+  }
+} 
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0 && numero % 5 === 0) {
+    return "fizzbuzz";
+  } else if(numero % 3 === 0) {
+    return "fizz";
+  } else if (numero % 5 === 0) {
+    return "buzz";
+  }else {
+    return numero;
+  }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -128,7 +141,23 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  } 
+  else if (num1 === 0 || num2 === 0 || num3 ===0) {
+    return "Error";
+  } 
+  else if (num3 > num1 && num3 > num2) {
+    return (num3 + 1);
+  } 
+  else if(num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
+  } 
+  else {
+    return false;
+  }
 }
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -136,6 +165,7 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
 }
 
 function esVerdadero(valor){
@@ -155,7 +185,11 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero.length == 3) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function doWhile(numero) {
