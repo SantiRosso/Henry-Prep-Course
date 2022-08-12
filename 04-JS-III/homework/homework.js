b570703c4546fa1a55a680bbea973947924f3778
Santiago Rosso <santiagorosso@live.com.ar>
2022-08-12 18:18:47 -0300

    14/21

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -119,13 +119,31 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var a = 1;
+  if (arguments.length === 0) {
+    return 0;
+  } else if(arguments.length === 1) {
+    return arguments[0];
+  }else {
+  for (var i = 0; i < arguments.length; i++) {
+    a = a * arguments[i];
+  }
+  return a;
+}
+
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var z = 0;
+for(var i = 0; i < arreglo.length; i++){
+  if (arreglo[i] > 18) {
+    z = z + 1;
+  } 
+}
+return z;
 }
 
 
@@ -134,7 +152,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia === 1 || numeroDeDia === 7) {
+    return "Es fin de semana";
+  } else {
+    return "Es dia Laboral";
+  }
 } 
 
 
